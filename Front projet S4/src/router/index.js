e0632ff420ee691ebc4676cd893abb2c71d0cba2
mesrlne <126901459+mesrlne@users.noreply.github.com>
2024-02-01 18:39:34 +0100

    ajout connexion pété
    Test d'ajout de la page connexion mais c'est pété

--- Front projet S4/src/router/index.js
+++ Front projet S4/src/router/index.js
@@ -1,29 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-    path: '/test',
-      name: 'test',
-      component: () => import('../views/test.vue')
+      path: "/connexionV2",
+      name: "connexionV2",
+      component: () => import("../views/connexionV2.vue"),
     },
     {
-      path: '/test_calendar',
-        name: 'test_calendar',
-        component: () => import('../views/test_calendar.vue')
+      path: "/test",
+      name: "test",
+      component: () => import("../views/test.vue"),
     },
     {
-      path: '/connexion',
-      name : 'connexion',
-      component: () => import('../views/connexion.vue')
+      path: "/test_calendar",
+      name: "test_calendar",
+      component: () => import("../views/test_calendar.vue"),
     },
     {
-      path: '/item_shop',
-      name : 'item_shop',
-      component: () => import('../views/item_shop.vue')
-    }
-  ]
-})
+      path: "/connexion",
+      name: "connexion",
+      component: () => import("../views/connexion.vue"),
+    },
+    {
+      path: "/item_shop",
+      name: "item_shop",
+      component: () => import("../views/item_shop.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ Front projet S4/src/views/connexionV2.vue
@@ -0,0 +1,220 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from "vue-router";
+</script>
+
+<template>
+    <div class="content">
+      <div class="login-container">
+        <h1>Bienvenue !</h1>
+        <form method="post">
+          <div class="txt_field">
+            <input type="text" required />
+            <span></span>
+            <label>Username</label>
+          </div>
+          <div class="txt_field">
+            <input type="password" required />
+            <span></span>
+            <label>Password</label>
+          </div>
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+      <section class="slide_container">
+        <div class="slide_wrapper">
+          <div class="slider">
+            <img
+              id="slide_1"
+              src="/images/camera0.jpeg"
+              alt="Image représentant 2 appareils photo"
+            />
+            <img
+              id="slide_2"
+              src="/images/camera1.jpeg"
+              alt="Représentation d'une caméra professionnelle utilisée lors de concerts"
+            />
+            <img
+              id="slide_3"
+              src="/images/camera2.jpeg"
+              alt="Image contentant un drone permetant de faire de la vidéo"
+            />
+            <div class="slider_nav">
+              <a href="#slide_1"></a>
+              <a href="#slide_2"></a>
+              <a href="#slide_3"></a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+</template>
+
+<style scoped>
+
+
+template {
+  background-color: #fb946c;
+  width: 100%;
+  min-height: 100vh;
+  overflow: hidden;
+  background-color: #fb946c;
+  padding: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.content {
+  display: flex;
+  background-color: aliceblue;
+  height: 450px;
+  width: 700px;
+  border-radius: 8%;
+}
+
+.login-container {
+  padding-top: 20px;
+  flex: 1;
+  height: 450px;
+  border-radius: 8%;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+}
+
+.login-container h1 {
+  text-align: center;
+  padding: 0 0 20px 0;
+}
+
+.login-container form {
+  padding: 0 40px;
+  margin: 30px 0;
+}
+
+form .txt_field {
+  position: relative;
+  border-bottom: 2px solid #adadad;
+  margin: 30px 0;
+}
+
+.txt_field input {
+  margin-top: 10px;
+  width: 100%;
+  padding: 0 5px;
+  height: 40px;
+  font-size: 16px;
+  border: none;
+  background: none;
+  outline: none;
+}
+
+.txt_field label {
+  position: absolute;
+  top: 50%;
+  left: 5px;
+  color: #adadad;
+  transform: translateY(-50%);
+  font-size: 16px;
+  pointer-events: none;
+  transition: 0.5s;
+}
+
+.txt_field span::before {
+  margin-top: 10px;
+  content: "";
+  position: absolute;
+  top: 40px;
+  left: 0;
+  width: 0%;
+  height: 2px;
+  background: #1a1a1a;
+  transition: 0.5s;
+}
+
+.txt_field input:focus ~ label,
+.txt_field input:valid ~ label {
+  margin-top: 10px;
+  top: -5px;
+  color: #1a1a1a;
+}
+
+.txt_field input:focus ~ span::before,
+.txt_field input:valid ~ span::before {
+  width: 100%;
+}
+
+input[type="submit"] {
+  margin-top: 10%;
+  width: 100%;
+  height: 50px;
+  border: 1px solid;
+  background: #1a1a1a;
+  border-radius: 15px;
+  font-size: 18px;
+  color: #e9f4fb;
+  font-weight: 700;
+  cursor: pointer;
+  outline: none;
+}
+
+input[type="submit"]:hover {
+  border-color: #1a1a1a;
+  transition: 0.5s;
+}
+
+.slide_container {
+  padding: 2rem;
+  width: 200px;
+  height: 350px;
+  flex: 1;
+  border-radius: 8%;
+  background-color: #fae4d6;
+  margin: 20px 20px 20px 0;
+}
+
+.slider_wrapper {
+  position: relative;
+  max-width: 48rem;
+  margin: 0 auto;
+}
+
+.slider {
+  display: flex;
+  aspect-ratio: 16/9;
+  overflow: auto;
+  scroll-snap-type: x mandatory;
+  scroll-behavior: smooth;
+  box-shadow: 0 1.5rem 3rem -0.75rem hsl(0, 0%, 0%, 0.25);
+}
+
+.slider img {
+  flex: 1 0 100%;
+  scroll-snap-align: start;
+  object-fit: cover;
+}
+
+.slider_nav {
+  display: flex;
+  column-gap: 1rem;
+  position: absolute;
+  bottom: 7.25rem;
+  left: 62%;
+  transform: translateX(-50%);
+  z-index: 1;
+}
+
+.slider_nav a {
+  width: 0.5rem;
+  height: 0.5rem;
+  border-radius: 50%;
+  background-color: #fff;
+  opacity: 0.75;
+  transition: opacity ease 250ms;
+}
+
+.slider_nav a:hover {
+  opacity: 1;
+}
+</style>
