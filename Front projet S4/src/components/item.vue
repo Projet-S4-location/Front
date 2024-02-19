b9e03807b1eab932c752fe4943e940f03d45c955
moiroudelliott <73348672+moiroudelliott@users.noreply.github.com>
2024-02-19 17:51:10 +0100

    add items in shop

---
+++ Front projet S4/src/components/item.vue
@@ -0,0 +1,12 @@
+<script setup>
+    const props = defineProps(["name", "tags", "prix", "image"])
+</script>
+
+<template>
+    <div>
+        <h1>{{props.name}}</h1>
+        <a v-for="tag in props.tags">{{ tag }}</a>
+        <h2>{{props.prix}}€/jour</h2>
+        <img :src = "props.image">
+    </div>
+</template>
