dbc507c6da5b6b0336bec59cc2b1afe207f89c14
ElianGARRIGUES <elian.garrigues@hotmail.fr>
2024-02-23 20:42:09 +0100

    Update connexion.vue

--- Front projet S4/src/views/connexion.vue
+++ Front projet S4/src/views/connexion.vue
@@ -9,11 +9,24 @@ const userStore = useUserStore();
 
 const username = ref('')
 const password = ref('')
+function checkCookiesEnabled() {
+    var cookieEnabled = navigator.cookieEnabled;
+    if (cookieEnabled === undefined) {
+        var cookieEnabled = true;
+        var testCookie = document.createElement("cookie");
+        testCookie.setAttribute("name", "testcookie");
+        document.body.appendChild(testCookie);
+        cookieEnabled = testCookie.value !== "";
+        document.body.removeChild(testCookie);
+    }
+    return cookieEnabled;
+}
+console.log(checkCookiesEnabled());
 async function envoi() {
   console.log(username.value, password.value)
   await fetch("http://51.68.91.213/info9/Back/users/connexion.php", {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
     credentials: 'include',
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
     body: JSON.stringify({
 	  "username": username.value,
     "password": password.value
@@ -31,14 +44,17 @@ async function envoi() {
         password.value = ''
         fetch("http://51.68.91.213/info9/Back/get_session.php", {
           credentials: 'include'
-        }).then((Response)=>{
-          return Response.json()}).then((data)=>{
-            console.log(data)
-            router.push("item_shop")
-
-          })
-
         })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        router.push("item_shop")
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+
+    })
 
     }
     else{
